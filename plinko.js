aebb8e5dd129d1f9bba7cb4d1b69e40faa674f3f
KrishAGARWA <73591403+KrishAGARWA@users.noreply.github.com>
2020-12-29 14:22:29 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,33 @@
+class Pp{
+constructor(x,y,r){
+   var options={
+isStatic:true,
+restitution:0.3,
+friction:0.5,
+density:1.2
+
+
+   }
+  this.r=r
+this.body=Bodies.circle(x,y,this.r,options)
+
+ 
+
+World.add(world,this.body)
+
+
+}
+display(){
+    fill("red")
+var pos=this.body.position
+ellipse(pos.x,pos.y,this.r,this.r)
+
+
+
+
+}
+
+
+
+
+}
